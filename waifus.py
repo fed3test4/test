ca32e2dfbe2d7116df8890eff58c420177f01dea
federico.testa <federico.testa97@edu.unito.it>
2023-08-25 15:10:15 +0900

    l

---
+++ waifus.py
@@ -0,0 +1,11 @@
+
+class waifu:
+    def __init__(self, name, chest, waist, hips):
+        self.name = name
+        self.chest = chest
+        self.waist = waist
+        self.hips = hips
+    
+    
+    def __str__(self):
+        return f'My name is {self.name}'
